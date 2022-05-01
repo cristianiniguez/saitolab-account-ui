7ecae2de76111b9112964d5171c77b537070a0a3
cristianiniguez <felizpc1@gmail.com>
2022-05-01 15:20:13 -0400

    feature/SA-1 Added formik to sign-up page

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Field, Form, Formik } from 'formik';
 import {
   Flex,
   Box,
@@ -18,8 +19,30 @@ import {
 } from '@chakra-ui/react';
 import { FiEye, FiEyeOff } from 'react-icons/fi';
 
+import type { FormikConfig } from 'formik';
+
+type SignUpFormValues = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+};
+
+type SignUpFormProps = FormikConfig<SignUpFormValues>;
+
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
+
+  const initialValues: SignUpFormValues = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  };
+
+  const handleSubmit: SignUpFormProps['onSubmit'] = (values) => {
+    console.log(values);
+  };
 
   return (
     <Flex
@@ -37,60 +60,83 @@ const SignUpPage = () => {
             to enjoy all of our cool features ✌️
           </Text>
         </Stack>
-        <Box rounded='lg' bg={useColorModeValue('white', 'gray.700')} boxShadow='lg' p={8}>
-          <Stack spacing={4}>
-            <HStack>
-              <Box>
-                <FormControl id='firstName' isRequired>
-                  <FormLabel>First Name</FormLabel>
-                  <Input type='text' />
-                </FormControl>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+          {() => (
+            <Form>
+              <Box rounded='lg' bg={useColorModeValue('white', 'gray.700')} boxShadow='lg' p={8}>
+                <Stack spacing={4}>
+                  <HStack>
+                    <Box>
+                      <Field name='firstName'>
+                        {({ field }) => (
+                          <FormControl id='firstName' isRequired>
+                            <FormLabel>First Name</FormLabel>
+                            <Input {...field} type='text' />
+                          </FormControl>
+                        )}
+                      </Field>
+                    </Box>
+                    <Box>
+                      <Field name='lastName'>
+                        {({ field }) => (
+                          <FormControl id='lastName' isRequired>
+                            <FormLabel>Last Name</FormLabel>
+                            <Input {...field} type='text' />
+                          </FormControl>
+                        )}
+                      </Field>
+                    </Box>
+                  </HStack>
+                  <Field name='email'>
+                    {({ field }) => (
+                      <FormControl id='email' isRequired>
+                        <FormLabel>Email address</FormLabel>
+                        <Input {...field} type='email' />
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name='password'>
+                    {({ field }) => (
+                      <FormControl id='password' isRequired>
+                        <FormLabel>Password</FormLabel>
+                        <InputGroup>
+                          <Input {...field} type={showPassword ? 'text' : 'password'} />
+                          <InputRightElement h='full'>
+                            <Button
+                              variant='ghost'
+                              onClick={() => setShowPassword((showPassword) => !showPassword)}
+                            >
+                              <Icon as={showPassword ? FiEye : FiEyeOff} />
+                            </Button>
+                          </InputRightElement>
+                        </InputGroup>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Stack spacing={10} pt={2}>
+                    <Button
+                      type='submit'
+                      loadingText='Submitting'
+                      size='lg'
+                      bg={'blue.400'}
+                      color='white'
+                      _hover={{
+                        bg: 'blue.500',
+                      }}
+                    >
+                      Sign up
+                    </Button>
+                  </Stack>
+                  <Stack pt={6}>
+                    <Text align='center'>
+                      Already a user? <Link color={'blue.400'}>Login</Link>
+                    </Text>
+                  </Stack>
+                </Stack>
               </Box>
-              <Box>
-                <FormControl id='lastName'>
-                  <FormLabel>Last Name</FormLabel>
-                  <Input type='text' />
-                </FormControl>
-              </Box>
-            </HStack>
-            <FormControl id='email' isRequired>
-              <FormLabel>Email address</FormLabel>
-              <Input type='email' />
-            </FormControl>
-            <FormControl id='password' isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} />
-                <InputRightElement h='full'>
-                  <Button
-                    variant='ghost'
-                    onClick={() => setShowPassword((showPassword) => !showPassword)}
-                  >
-                    <Icon as={showPassword ? FiEye : FiEyeOff} />
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                loadingText='Submitting'
-                size='lg'
-                bg={'blue.400'}
-                color='white'
-                _hover={{
-                  bg: 'blue.500',
-                }}
-              >
-                Sign up
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align='center'>
-                Already a user? <Link color={'blue.400'}>Login</Link>
-              </Text>
-            </Stack>
-          </Stack>
-        </Box>
+            </Form>
+          )}
+        </Formik>
       </Stack>
     </Flex>
   );
