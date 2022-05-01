5d336c1afcbf27eed1ca359869b6c884c3e5dd6e
cristianiniguez <felizpc1@gmail.com>
2022-05-01 14:19:18 -0400

    feature/SA-1 Templates for sign-in and sign-up pages added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import { Box, Button, Center, Heading, HStack } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+
+  const goToSignInPage = () => router.push('/sign-in');
+  const goToSignUpPage = () => router.push('/sign-up');
+
   return (
     <Center h='100vh'>
       <Box>
         <Heading mb={8}>Welcome to Saito Lab Accounts</Heading>
         <HStack spacing={8} justifyContent='center'>
-          <Button>Sign In</Button>
-          <Button>Sign Up</Button>
+          <Button onClick={goToSignInPage}>Sign In</Button>
+          <Button onClick={goToSignUpPage}>Sign Up</Button>
         </HStack>
       </Box>
     </Center>
