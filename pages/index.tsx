51f23c385c80d79ecc9224b1c5e5f2e00e9d38e0
Cristian Iñiguez <felizpc1@gmail.com>
2022-05-30 21:07:16 -0400

    feature/SA-7 next-intl installed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
+import { GetServerSideProps } from 'next';
 import { Box, Button, Center, Heading, HStack } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
+import { useTranslations } from 'next-intl';
 
 import Layout from 'components/others/Layout';
 import { ROUTES } from 'constants/';
 
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  return {
+    props: {
+      messages: (await import(`i18n/${locale}.json`)).default,
+    },
+  };
+};
+
 export default function Home() {
   const router = useRouter();
+  const t = useTranslations();
 
   const goToSignInPage = () => router.push(ROUTES.SIGN_IN);
   const goToSignUpPage = () => router.push(ROUTES.SIGN_UP);
@@ -14,7 +25,7 @@ export default function Home() {
     <Layout>
       <Center h='100vh'>
         <Box>
-          <Heading mb={8}>Welcome to Saito Lab Accounts</Heading>
+          <Heading mb={8}>{t('welcome')}</Heading>
           <HStack justifyContent='center' spacing={8}>
             <Button onClick={goToSignInPage}>Sign In</Button>
             <Button onClick={goToSignUpPage}>Sign Up</Button>
