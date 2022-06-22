28af9697febb4140bbadaebd79ff25556ac8d6b0
Cristian Iñiguez <felizpc1@gmail.com>
2022-06-22 15:41:05 -0400

    feature/SA-13 Using brand colors on layout

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
 import { useTranslations } from 'next-intl';
-import { Flex, Stack, Heading, Text, useColorModeValue } from '@chakra-ui/react';
+import { Stack, Heading, Text, Center } from '@chakra-ui/react';
 
 import Layout from 'components/others/Layout';
 import Favicon from 'components/others/Favicon';
@@ -33,12 +33,7 @@ const SignUpPage = () => {
 
   return (
     <Layout title={t('signUp.pageTitle')}>
-      <Flex
-        align='center'
-        bg={useColorModeValue('gray.50', 'gray.800')}
-        justify='center'
-        minH='100vh'
-      >
+      <Center flex={1}>
         <Stack maxW='lg' mx='auto' px={6} py={12} spacing={8}>
           <Favicon mx='auto' />
           <Stack align='center'>
@@ -51,7 +46,7 @@ const SignUpPage = () => {
           </Stack>
           <SignUpForm />
         </Stack>
-      </Flex>
+      </Center>
     </Layout>
   );
 };
