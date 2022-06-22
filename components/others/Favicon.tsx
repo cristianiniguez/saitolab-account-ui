a3553f360e7ee7ea541955d4664ef637493599cc
Cristian Iñiguez <felizpc1@gmail.com>
2022-06-22 15:16:06 -0400

    feature/SA-13 Enabling chakra svg props on Favicon

--- components/others/Favicon.tsx
+++ components/others/Favicon.tsx
@@ -1,11 +1,14 @@
+import { ComponentProps, FC } from 'react';
 import { chakra, useTheme } from '@chakra-ui/react';
 
-const Favicon = () => {
+type FaviconProps = ComponentProps<typeof chakra.svg>;
+
+const Favicon: FC<FaviconProps> = props => {
   const theme = useTheme();
   const { black, green, yellow } = theme.colors;
 
   return (
-    <chakra.svg boxSize={16} viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'>
+    <chakra.svg boxSize={16} viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg' {...props}>
       <rect fill={green[900]} height={64} rx={8} width={64} />
       <circle cx={32} cy={32} fill={yellow[500]} r={24} stroke={black} />
       <path d='M30 50H34V14L30 14V50Z' fill={yellow[600]} stroke={black} strokeLinejoin='round' />
